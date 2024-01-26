ed53476002572b5950eece69489388761c7f19de
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-25 17:06:31 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_TryCatchAdam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
