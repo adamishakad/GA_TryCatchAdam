eed58b0ddd23dbd43ab8e6be5ea489af4f85b377
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-25 22:38:16 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace GA_TryCatchAdam
+﻿// // Adam
+
+
+namespace GA_TryCatchAdam
 {
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please inter your age");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int age = int.Parse(userInput);
+
+                Console.WriteLine("Welcome you are " + age + " years old");
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input.Please input valid integer for age");
+                Console.ReadLine();
+            }
+
         }
     }
 }
